a3f1ede4f115bfd15ba41c5fedd64c578442bd0d
Guilherme Santos <g.santos.sfc@gmail.com>
2021-04-21 12:30:41 -0300

    add component Player and mock with json-server

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,12 @@ import styles from './styles.module.scss';
 
 export function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMM', {
-    locale: ptBR
+    locale: ptBR,
   });
-  
+
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr"/>
+      <img src="/logo.svg" alt="Podcastr" />
 
       <p>O melhor para você ouvir, sempre</p>
 
